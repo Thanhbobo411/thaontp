2800c7409ec98532015d21194182f60232adc3ca
ThanhNV <thanhnv@mpos.vn>
2023-03-31 14:52:32 +0700

    update code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 //        1/ login
-//        loginForm("test", "test");
+//        loginForm("test", "testa");
 //        2/ handle checkboxes
 //        handleCheckboxes();
         //3/ handle dropdown list
