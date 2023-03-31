505644e78774b736d3efe5262dc1645fbba37d36
Thaontp <thaontp@gmail.com>
2023-03-31 15:30:57 +0700

    demo git

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,15 @@
 package org.example;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.List;
 
 public class Main {
@@ -15,8 +19,8 @@ public class Main {
 //        2/ handle checkboxes
 //        handleCheckboxes();
         //3/ handle dropdown list
-        System.out.println("hello world aaaaa");
-        handleDropdownList();
+//        handleDropdownList();
+        handleAlerts();
 
     }
     public static void loginForm(String user, String pass) throws InterruptedException {
@@ -54,6 +58,24 @@ public class Main {
         Select select = new Select(listItems);
         select.selectByVisibleText("Option 1");
         Thread.sleep(1000);
+        driver.quit();
+    }
+
+    public static void handleAlerts() throws InterruptedException {
+        WebDriver driver = InitBrowser.InitChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement jsAlertItem = driver.findElement(By.cssSelector("[onclick='jsAlert()']"));
+        WebElement resultContent = driver.findElement(By.id("result"));
+
+        jsAlertItem.click();
+        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+        System.out.println("Alert content: " + alert.getText());
+        alert.accept();
+        System.out.println("Result: "+ resultContent.getText());
+        System.out.println("-----");
+
+        Thread.sleep(2000);
         driver.quit();
     }
 }
